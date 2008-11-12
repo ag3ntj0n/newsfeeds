34d3c2a4b0faf3f3ae7411363500bc639510b01e
Jon <agentjon@gmail.com>
2008-11-12 08:20:56 -0500

    gotrid of junk

--- src/main/scala/newsfeeds/aggregate/OpmlFeed.scala
+++ src/main/scala/newsfeeds/aggregate/OpmlFeed.scala
@@ -10,13 +10,8 @@ class OpmlFeed(var feedpath:String) extends Feed {
   private val feed:Elem =  XML.load(feedpath)
   override val name =  (feed \ "head" \ "title")
   
-  //var itms:NodeSeq =   new Queue[Elem]
-  //(feed \\ "outline").foreach( entry => if ((entry \ "@xmlUrl").length  == 1 ) {itms = itms ++ entry} )
-  //val i = (feed \\ "outline").filter( k => (k \ "@xmlUrl").length == 1 )
-  //if ( itms== i) {println("!!!!!!!!!!!!!!!!!!!!")} else {println("doh")}
-  
-  val items =  (feed \\ "outline").filter( k => (k \ "@xmlUrl").length == 1 )// itms.asInstanceOf[NodeSeq] //(feed \\ "outline").foreach () //where outline has xmlUrl
-  override val articles = items.map( x => new OpmlItem(x) )//Seq.empty
+  val items =  (feed \\ "outline").filter( k => (k \ "@xmlUrl").length == 1 )
+  override val articles = items.map( x => new OpmlItem(x) )
 }
 
 class OpmlItem( n:Node ) extends TItem{
